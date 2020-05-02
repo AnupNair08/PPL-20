d55f2238485137a924b300e75d739d143adc7378
Anup Nair <nair.anup09>
2020-05-02 14:05:19 +0530

    Assignment 1 updates

--- Assignment 1 (Python)/6.py
+++ Assignment 1 (Python)/6.py
@@ -1,28 +1,25 @@
-t = int(input())
-while(t):
-    l = [int(i) for i in input().split()]
-    n = l[0]
-    s = l[1]
-    price = [int(i) for i in input().split()]
-    tp = [int(i) for i in input().split()]
+from math import sqrt
+def sod(a):
+    sum = 0
+    for i in range(2,int(sqrt(a)) + 1):
+        if(a % i == 0):
+            sum += i
+            if(a // i != a % (a // i)):
+                sum += a//i 
+    return sum + 1
 
-    m1 = min(price)
-    i = price.index(m1)
-    s += m1
-    if(i == 0):
-        ck = 1
+
+print("First 10 amicable numbers are")
+count = 0
+i = 1
+l = 0
+while(count < 10):
+    k = sod(i)
+    if(sod(k) == i and i != k):
+        print(i,k)
+        l = k
+        count += 1
+    if(i == l - 1):
+        i += 2
     else:
-        ck = 0
-    p = []
-    for i in range(len(price)):
-        if(tp[i] == ck):
-            p.append(price[i])
-
-    s += min(p)
-    # print(s)
-    if(s <= 100):
-        print("yes")
-    else:
-        print("no")
-    t -= 1
-
+        i += 1
--- Assignment 1 (Python)/9.py
+++ Assignment 1 (Python)/9.py
@@ -1,20 +1,30 @@
 from math import sqrt
-def divisor(a):
-    count = 0
-    sum = 0
-    for i in range(1,int(sqrt(a))):
-        if( a % i == 0 and i != a//i):
-            sum += 1 / i
-            sum += i / a
-            count += 2
-        elif( a % i == 0 and i == a// i):
-            sum += 1 / i
-            count += 1
-    
-    print(count,sum)
+def div(a):
+    num = 0
+    den = 0
+    for i in range(1,int(sqrt(a)) + 1):
+        if(a % i == 0):
+            # print(i, a / i)
+            den += 1/i
+            num += 1
+            if(a // i != a % (a // i)):
+                den += 1/ (a//i)
+                num += 1
+    k = num / den
+    k = round(k,4)
+    if (k.is_integer()):
+        return True
+    else:
+        return False
 
 
-i = 0
-j = 1
-for i in range(10):
-    divisor(j)
+
+print("First 10 Harmonic Divisor numbers are :")
+count = 0
+i = 1
+
+while(count < 10):
+    if(div(i) == True):
+        print(i)
+        count += 1
+    i += 1
